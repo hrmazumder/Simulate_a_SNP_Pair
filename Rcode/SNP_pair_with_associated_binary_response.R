96a44c0079e6f373bd52db5d3f1d9433ddcddab7
Harun Mazumder <72481906+hrmazumder@users.noreply.github.com>
2021-06-27 14:41:44 -0500

    Update SNP_pair_with_associated_binary_response.R

--- Rcode/SNP_pair_with_associated_binary_response.R
+++ Rcode/SNP_pair_with_associated_binary_response.R
@@ -8,32 +8,28 @@
 sim_true_snp = function(snp1_name, snp2_name, sam_size, size, geno, prob, prop_y){
 
                 samples = rmultinom(n = sam_size , size = size, prob = prob)
-                cell_cat = apply(X = samples, MARGIN = 2, FUN = function(x){ which.max(x) })
+                cell_cat = apply(X = samples, MARGIN = 2, FUN = function(x){which.max(x)} )
                 list_final_geno = lapply(cell_cat, FUN = function(x, y){ y[x] } , y =  geno)
                 snp1_snp2 = do.call(rbind, Map(rbind, list_final_geno))
                 
-                dist = as.data.frame(table(snp1_snp2)) #distn of genotype combns 
-                dat = NULL #to generate binary response and bind  with snp1_snp2
+                dist = as.data.frame(table(snp1_snp2)) #distn of genotype combinations  
+                dat = NULL #to store binary response correaponding to 'geno' and bind with 'snp1_snp2'
                 
-            for(i in 1:length(geno)){
-         
-                #use trycatch to handle error
-                skip_to_next <- FALSE
-                tryCatch({ 
-                dat =  rbind(dat, cbind(rbinom(n = dist$Freq[dist$snp1_snp2 == geno[i]] ,size = 1, prob = prop_y[i]),
-                            snp1_snp2[snp1_snp2==geno[i]]) )
-                 }, 
-                 error = function(e) {skip_to_next <<- TRUE} ) 
-            if(skip_to_next) {next} 
-               }  
+             for(i in 1:length(geno)){
+                dat =  rbind(dat, 
+                             cbind(rbinom(n = dist$Freq[dist$snp1_snp2 == geno[i]], size = 1, prob = prop_y[i]),
+                                   snp1_snp2[snp1_snp2==geno[i]]) )
+                  }  
              
-                snp1 = sub("-.*", "", dat[,2])
+                snp1 = sub("-.*", "", dat[,2]) 
                 snp2 = sub(".*-", "", dat[,2])
-                final_dat = data.frame(as.numeric(as.character(dat[,1])), snp1, snp2)
-                colnames(final_dat) = c("response", snp1_name, snp2_name)
-                final_dat$rand = runif(n = dim(final_dat)[1]) #randomization
-                final_dat = final_dat[order(final_dat$rand),]
-                return(final_dat[, -4])
+                dat2 = data.frame(as.numeric(as.character(dat[,1])), snp1, snp2) #ensure response = dat[,1] is numerical
+                colnames(dat2) = c("response", snp1_name, snp2_name)
+                
+                #now randomly shuffle the observations in dat2 (optional) 
+                n =  dim(dat2)[1]
+                final_dat = dat2[sample(1:n, size = n, replace = F), ]
+                return(final_dat)
                 }
 #function ends
 
@@ -50,21 +46,21 @@ set.seed(100)
 geno = c("GG-AA", "GG-AG", "GG-GG", "GA-AA", "GA-AG", "GA-GG", "AA-AA", "AA-AG", "AA-GG")  
 
 #sample size in real data
-sample_size = 18000 
+sam_size = 18000 #sample size
 
 #proportion in each genotype combination (or cell probabilities):
-prob = c(3641, 3864, 1075, 3242, 3485, 954, 736, 770, 233)/sample_size 
+prob = c(3641, 3864, 1075, 3242, 3485, 954, 736, 770, 233)/sam_size 
 
 #proportion of y=1 in each cell:
 prop_y = c(0.22, 0.22, 0.19, 0.26, 0.22, 0.21, 0.26, 0.21, 0.21)
 
-df = sim_true_snp(snp1_name="SNP1", snp2_name="SNP2", sam_size = sample_size, size=1, geno=geno, prob=prob, prop_y=prop_y)
+df = sim_true_snp(snp1_name="SNP1", snp2_name="SNP2", sam_size = sam_size, size=1, geno=geno, prob=prob, prop_y=prop_y)
                   
 head(df)
-     response SNP1 SNP2
-3753         0   GG   AG
-5843         0   GG   AG
-3947         0   GG   AG
-10921        0   GA   AA
-3605         0   GG   AA
-10153        0   GA   AA
+      response SNP1 SNP2
+14497        0   GA   AG
+5406         0   GG   AG
+17952        0   AA   GG
+13232        0   GA   AG
+405          0   GG   AA
+5772         1   GG   AG

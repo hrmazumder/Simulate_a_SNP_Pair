db32d9759a683b9b6979a2a20da81cafd935f4c4
Harun Mazumder <72481906+hrmazumder@users.noreply.github.com>
2023-11-18 16:42:57 -0600

    Update SNP_pair_with_associated_binary_response.R

--- Rcode/SNP_pair_with_associated_binary_response.R
+++ Rcode/SNP_pair_with_associated_binary_response.R
@@ -6,8 +6,6 @@
 #' @parameter snp1_name: provide SNP1 name you want to see in the simulated data.
 #' @parameter snp2_name: provide SNP2 name you want to see in the simulated data.
 #' @parameter sam_size: sample size of the real data (also sample size of the simulated data)
-#' @parameter size : total number of objects that are put into cells in the typical multinomial experiment.
-                        #if size=1 in rnultinom(), it will return 0s and one 1 which is indicator of the cell is occured/picked up as result. 
 #' @parameter geno: a vector of combination of genotypes of SNP1 and SNP2, call "cells" in a Multinomial distribution. 
 #' @parameter prob: a vector of probabilities (or proportion of counts) in cells.
 #' @parameter prop_y: a vector of success probabilities of binary coutcome (y), i.e., each element of prop_y is proporiton of y=1 in each cell.
@@ -16,11 +14,12 @@
 #' @author Harun Mazumder
 
 
-#function to generate true SNP pair starts:
+#function to generate a SNP pair starts:
 
-sim_true_snp = function(snp1_name, snp2_name, sam_size=1, size, geno, prob, prop_y){
+sim_true_snp = function(snp1_name, snp2_name, sam_size, geno, prob, prop_y){
 
-                samples = rmultinom(n = sam_size , size = size, prob = prob)
+                samples = rmultinom(n = sam_size , size = 1, prob = prob) 
+                #samples: indicator variables (0,1); 1 indicating the category most likely to occur in each observation
                 cell_cat = apply(X = samples, MARGIN = 2, FUN = function(x){which.max(x)} )
                 list_final_geno = lapply(cell_cat, FUN = function(x, y){ y[x] } , y =  geno)
                 snp1_snp2 = do.call(rbind, Map(rbind, list_final_geno))
@@ -66,7 +65,7 @@ prob = c(3641, 3864, 1075, 3242, 3485, 954, 736, 770, 233)/sam_size
 #proportion of outcome=1 in each cell:
 prop_y = c(0.22, 0.22, 0.19, 0.26, 0.22, 0.21, 0.26, 0.21, 0.21)
 
-df = sim_true_snp(snp1_name="SNP1", snp2_name="SNP2", sam_size = sam_size, size=1, geno=geno, prob=prob, prop_y=prop_y)
+df = sim_true_snp(snp1_name="SNP1", snp2_name="SNP2", sam_size = sam_size, geno=geno, prob=prob, prop_y=prop_y)
                   
 head(df)
        outcome SNP1 SNP2
